4d2d1bf08a64d4e5aab4597a1ee91d4e152ef9c4
JonaD12 <35873930+JonaD12@users.noreply.github.com>
2018-02-24 13:35:38 -0600

    punto 2
    actualizacion del punto 2 acerca del parcial

--- index.js
+++ index.js
@@ -7,7 +7,24 @@ const request = require('request');
 
 bot.on('message', (msg) => {
 var Hola = "/hola";
+
 if (msg.text.toString().toLowerCase().indexOf(Hola) === 0) {
     bot.sendMessage(msg.chat.id, "Hola  " + msg.from.first_name);
 }
+else if  (msg.text.toString().toLowerCase().indexOf("r") > -1) {
+    bot.sendMessage(msg.chat.id, "no te conozco  " );
+}
+else if  (msg.text.toString().toLowerCase().indexOf("z") > -1) {
+    bot.sendMessage(msg.chat.id, "no te conozco  " );
+}
+else if  (msg.text.toString().toLowerCase().indexOf("h") > -1) {
+    bot.sendMessage(msg.chat.id, "no te conozco  " );
+}
+else if  (msg.text.toString().toLowerCase().indexOf("k") > -1) {
+    bot.sendMessage(msg.chat.id, "no te conozco  " );
+}
+else if  (msg.text.toString().toLowerCase().indexOf("y") > -1) {
+    bot.sendMessage(msg.chat.id, "no te conozco  " );
+}
 });
+
